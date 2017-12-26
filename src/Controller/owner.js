fca7e5aaa2a77b8379db708ff09cb97aed0ca729
Andrew Presnell <andrew_presnell@hotmail.com>
2017-12-25 20:56:41 -0700

    Finalized First Round of Restful API

--- src/Controller/owner.js
+++ src/Controller/owner.js
@@ -4,6 +4,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var Owner = require("../Models/owner.js");
+var ParkingSpot = require("../Models/parkingSpot.js");
 
 // Route to post our 'Create Owner' form submission to mongoDB via mongoose
 route.post("/create-owner", function(req, res) {
@@ -39,7 +40,7 @@ route.post("/create-owner", function(req, res) {
 });
 
 route.get("/get-owners", function(req, res) {
-    Owner.find({}).populate("parkingSpots")
+    Owner.find({})//.populate("parkingSpots")
     .then(function(dbOwner) {
         res.json(dbOwner);
     })
@@ -48,10 +49,109 @@ route.get("/get-owners", function(req, res) {
     });
 });
 
-//put route
+route.get("/get-owner/:id", function(req, res) {
+    Owner.find({_id: req.params.id}).populate("parkingSpots")
+    .then(function(dbOwner) {
+        res.json(dbOwner);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
 
-route.put("/update-owner:id", function(req, res) {
-    Owner.find({_id:req.params._id})
-})
+//update Owner
+route.put("/update-owner", function(req, res) {
+    //create temporary object of request body
+    //then create address variable from temp objects address
+    //then remove whitespace from address variable
+    //then instantiate latitude and longitude properties of the temp object
+    let tempObject = req.body;
+    const address = tempObject.street + tempObject.city + tempObject.state + tempObject.zip;
+    address.replace(/ /g,'');
+    tempObject.lat = 0;
+    tempObject.lng = 0;
+
+    //use axios to pass address into google to return coordinates 
+    //then add those coordinates to the temp object
+    //then add that temp object into the db as an parking spot
+    axios.get("https://maps.google.com/maps/api/geocode/json?key=AIzaSyDu3uARDgsUWZTKOQ_CItX7_grlIU11Ieo&address=" + address)
+    .then(function(response) {
+        const coords = response.data.results[0].geometry.location;
+        tempObject.lat = coords.lat;
+        tempObject.lng = coords.lng;
+    }).then(function() {
+        // Update parking spot using the temp object
+        Owner.update({
+            _id:req.body._id
+        }, {
+            $set: {
+                firstName: tempObject.firstName,
+                lastName: tempObject.lastName,
+                email: tempObject.email,
+                phoneNumber: tempObject.phoneNumber,
+                street: tempObject.street,
+                city: tempObject.city,
+                state: tempObject.state,
+                zip: tempObject.zip,
+                lat: tempObject.lat,
+                lng: tempObject.lng
+            }
+        })
+        .then(function(dbOwner) {
+            res.json(dbOwner);
+        })
+        .catch(function(err) {
+            // If an error occurs, send the error to the client
+            res.json(err);
+        });
+    });
+});
+
+//Delete Owner
+route.get("/delete-owner/:id", function(req, res) {
+    //find the owner via id
+    Owner.findOne({_id: req.params.id})
+    .then((dbOwner) => {
+        //for each parking spot the owner has
+        dbOwner.parkingSpots.forEach(parkingSpotId => {
+            console.log(parkingSpotId);
+            //delete the actual parking spots
+            ParkingSpot.remove({_id: parkingSpotId}, function(err, removed) {
+                // Log any errors from mongojs
+                if (err) {
+                    console.log(err);
+                    res.send(err);
+                }
+                // Otherwise, send the mongojs response to the browser
+                // This will fire off the success function of the ajax request
+            });
+        });
+    })
+    //then clear the parking spots from the owner array
+    .then(() => {
+        Owner.update({_id: req.params.id}, {
+            $set: {
+                parkingSpots: []
+            }
+        }, function(err, removed) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            }
+        });
+    })
+    //then delete the actual owner
+    .then(() => {
+        Owner.remove({_id: req.params.id}, function(err, removed) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            else {
+                res.send(removed);
+            }
+        });
+    });
+});
 
 module.exports = route;
--- src/Controller/vehicle.js
+++ src/Controller/vehicle.js
@@ -35,12 +35,87 @@ route.post("/create-vehicle", function(req, res) {
 });
 
 route.get("/get-vehicles", function(req, res) {
-    Vehicle.find({})
+    Vehicle.find({}).populate("driverID")
     .then(function(dbVehicle) {
         res.json(dbVehicle);
     })
     .catch(function(err) {
         res.json(err);
+    });
+});
+
+route.get("/get-vehicle/:id", function(req, res) {
+    Vehicle.find({_id: req.params.id}).populate("driverID")
+    .then(function(dbVehicle) {
+        res.json(dbVehicle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+route.put("/update-vehicle", function(req, res) {
+    // Update parking spot using the temp object
+    vehicle.update({
+        _id:req.body._id
+    }, {
+        $set: {
+            make: req.body.make,
+            model: req.body.model,
+            year: req.body.year,
+            color: req.body.color,
+        }
+    })
+    .then(function(dbParkingSpot) {
+        res.json(dbParkingSpot);
+    })
+    .catch(function(err) {
+        // If an error occurs, send the error to the client
+        res.json(err);
+    });
+})
+
+route.get("/delete-vehicle/:id", function(req, res) {
+    //5a39d51be4888f04988d910d
+    //find the vehicle via id to get the driverID
+    Vehicle.find({_id: req.params.id}, () => {
+        //then find the owner of the vehicle
+        console.log("find vehicle");
+    })
+    .then((dbVehicle) => {
+        console.log("dbVehicleId");
+        console.log(dbVehicle[0].driverID);
+        Driver.update({
+            _id: dbVehicle[0].driverID
+        }, {
+            //then remove the vehicle id from the drivers vehicles array
+            $pull: {
+                vehicles: dbVehicle[0]._id
+            }
+        }, function(err, removed) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            }
+        });
+    })
+    //then delete the vehicle
+    .then(() => {
+        console.log("vehicle.remove");
+        Vehicle.remove({
+            _id: req.params.id
+        }, function(err, removed) {
+            // Log any errors from mongojs
+            if (err) {
+                console.log(err);
+                res.send(err);
+            }
+            // Otherwise, send the mongojs response to the browser
+            // This will fire off the success function of the ajax request
+            else {
+                res.send(removed);
+            }
+        });
     });
 });
 
