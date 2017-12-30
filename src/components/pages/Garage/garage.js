09828be8317be5e1a691da2f55ead51638e1b6c0
Darnell W <darnellwillis96@gmail.com>
2017-12-30 02:48:35 -0700

    additional pages added

---
+++ src/components/pages/Garage/garage.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Image from './../../../Images/parking.png'; 
+
+export default class Garage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="garage" style={{ background: 'green', marginLeft: '10%', width: '20%', height: '20%', borderRadius: '10%' }}>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'center' }}>Parking Space:  </h4>
+                    <img src={Image} style={{ width: '100%', height: 100 }} />
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Address: 92 W Vaugn Avenue </h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Rate: $7</h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Status:Vacant </h4>
+                </div>
+                <div className="garage" style={{ background: 'green', marginLeft: '10%', width: '20%', height: '20%', borderRadius: '10%' }}>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'center' }}>Parking Space:  </h4>
+                    <img src={Image} style={{ width: '100%', height: 100 }} />
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Address: 92 W Vaugn Avenue </h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Rate: $7</h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Status:Vacant </h4>
+                </div>
+                <div className="garage" style={{ background: 'green', marginLeft: '10%', width: '20%', height: '20%', borderRadius: '10%' }}>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'center' }}>Parking Space:  </h4>
+                    <img src={Image} style={{ width: '100%', height: 100 }} />
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Address: 92 W Vaugn Avenue </h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Rate: $7</h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Status:Vacant </h4>
+                </div>
+                <div className="garage" style={{ background: 'green', marginLeft: '10%', width: '20%', height: '20%', borderRadius: '10%', marginTop: '5%' }}>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'center' }}>Parking Space:  </h4>
+                    <img src={Image} style={{ width: '100%', height: 100 }} />
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Address: 92 W Vaugn Avenue </h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Rate: $7</h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Status:Vacant </h4>
+                </div>
+                <div className="garage" style={{ background: 'green', marginLeft: '10%', width: '20%', height: '20%', borderRadius: '10%', marginTop: '5%' }}>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'center' }}>Parking Space:  </h4>
+                    <img src={Image} style={{ width: '100%', height: 100 }} />
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Address: 92 W Vaugn Avenue </h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Rate: $7</h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Status:Vacant </h4>
+                </div>
+                <div className="garage" style={{ background: 'green', marginLeft: '10%', width: '20%', height: '20%', borderRadius: '10%', marginTop: '5%' }}>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'center' }}>Parking Space:  </h4>
+                    <img src={Image} style={{ width: '100%', height: 100 }} />
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Address: 92 W Vaugn Avenue </h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Rate: $7</h4>
+                    <h4 style={{ color: '#e6e6e6', textAlign: 'left', marginLeft: '5%' }}>Status:Vacant </h4>
+                </div></div>
+        );
+    }
+}
