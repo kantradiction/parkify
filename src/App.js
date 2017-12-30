33f736a3eab94426d175a0c4e6ab07201d453d14
Darnell W <darnellwillis96@gmail.com>
2017-12-29 18:01:33 -0700

    landing and dashboard page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
+import Landing from './components/pages/landing/landing';
+import Dashboard from './components/pages/dashboard/dashboard';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <p>This is a test to see if my branch works</p>
-        </p>
+      <div>
+      < Landing />
       </div>
     );
   }
